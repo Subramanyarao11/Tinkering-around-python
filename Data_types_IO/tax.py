0029060f3cf66ca5032a45b6e2dfaabffe4315a5
Subramanyarao11 <subramanya11rao@gmail.com>
2022-10-12 00:16:52 +0530

    mod-1

---
+++ Data_types_IO/tax.py
@@ -0,0 +1,16 @@
+# print("Hi Subramanya!")
+# Tax Calculator
+# amount = 10
+# tax = 0.06
+# total = str(amount + tax)
+# msg = ("Total Tax Amount is " + total + "$")
+# print(msg)
+
+
+# Improvized Version
+
+tax = 0.06
+amount = int(input("Enter the amount\n"))
+total = str(amount + tax)
+msg = ("Total Tax Amount is " + total + "$")
+print(msg)
